7f85bc26bf6510dfc2d4463a76be18def725964b
Efejotax <fquishpe6288@uta.edu.ec>
2021-06-28 22:44:35 -0500

    Pagina de Lesiones wachin anashei

--- src/routes/AppRutas.tsx
+++ src/routes/AppRutas.tsx
@@ -8,6 +8,7 @@ import {
     Link,
     Redirect
   } from "react-router-dom";
+import { LesionesPage } from '../pages/LesionesPage';
 
 export const AppRutas = () => {
     return (
@@ -16,7 +17,7 @@ export const AppRutas = () => {
        
         {/*Poner los enlaces antes de / porque sino solo se redirige a raiz*/}
         <Switch>
-          
+        <Route path='/lesiones' component={LesionesPage}/>
           <Route path='/piramide' component={PiramidePage}/>
           <Route path='/' component={InicioPage}/>
 
