c1069e5cc2fa95dfe2d364fa4b44382bbe710350
Sebastian <sebasmayorga98@gmail.com>
2021-06-28 22:45:03 -0500

    pagina de cargas

--- src/routes/AppRutas.tsx
+++ src/routes/AppRutas.tsx
@@ -8,6 +8,7 @@ import {
     Link,
     Redirect
   } from "react-router-dom";
+import { CargarDanio } from '../pages/CargarDanio';
 
 export const AppRutas = () => {
     return (
@@ -17,6 +18,7 @@ export const AppRutas = () => {
         {/*Poner los enlaces antes de / porque sino solo se redirige a raiz*/}
         <Switch>
           
+          <Route path='/cargaD' component={CargarDanio}/>
           <Route path='/piramide' component={PiramidePage}/>
           <Route path='/' component={InicioPage}/>
 
