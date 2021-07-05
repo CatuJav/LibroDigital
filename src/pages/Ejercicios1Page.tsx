b4822179108ad3474cb4f612d21fc48364c7b924
Javier <jalcoser1996@gmail.com>
2021-07-05 16:13:50 -0500

    Cambio de posición de los botones de avance

--- src/pages/Ejercicios1Page.tsx
+++ src/pages/Ejercicios1Page.tsx
@@ -1,81 +1,112 @@
-import React from 'react'
-import {useHistory } from "react-router-dom";
-import '../App.css';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "../App.css";
 import playImg from "../assets/play.png";
 import niniosImg from "../assets/ninios1.jpg";
 import video1 from "../assets/ejerciciovideo.mp4";
-import '../themes/estilos.css';
+import "../themes/estilos.css";
 export const Ejercicios1Page = () => {
-    const history=useHistory();
+  const history = useHistory();
 
-    const irAEjercicios2=()=>{
-        history.push('/ejercicios2');
-        //console.log('clic')
-      }
-      const irAEPaginaCatu=()=>{
-        history.push('/sana');
-        //console.log('clic')
-      }
+  const irAEjercicios2 = () => {
+    history.push("/ejercicios2");
+    //console.log('clic')
+  };
+  const irAEPaginaCatu = () => {
+    history.push("/sana");
+    //console.log('clic')
+  };
 
-    return (
-        <div className="fondo">
-            <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered modal-lg">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">IMPORTANCIA DEL EJERCICIO</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-      <video width="100%" id="videoEjercicio"  controls onPointerEnter={(e) => {
+  return (
+    <div className="fondo">
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                IMPORTANCIA DEL EJERCICIO
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <video
+                width="100%"
+                id="videoEjercicio"
+                controls
+                onPointerEnter={(e) => {
                   e.currentTarget.play();
                   console.log(e.currentTarget.ended);
                 }}
-
-                onPointerOut={(e)=>{
+                onPointerOut={(e) => {
                   e.currentTarget.pause();
-                }}>
-        <source src={video1} type="video/mp4"></source>
-      </video>
-     </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-primary" data-bs-dismiss="modal" >VOLVER</button>
-        
+                }}
+              >
+                <source src={video1} type="video/mp4"></source>
+              </video>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-dismiss="modal"
+              >
+                VOLVER
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="divtitulo">EJERCICIO</div>
+      <div className="divvideo">
+        <button
+          className="button"
+          style={{ fontSize: 15 }}
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          {" "}
+          <img src={playImg} width="15%" /> VER VIDEO
+        </button>
+      </div>
+      <div className="divcontenido">
+        <div className="divImagen">
+          <img src={niniosImg} width="100%" />
+        </div>
+        <div className="divf">
+          <div className="divtexto">
+            <br />
+            Para mantener un cuerpo sano es importante hacer ejercicio todos los
+            días
+          </div>
+        </div>
+      </div>
+      <div
+        className="container"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <div className="">
+          <button className="button" onClick={irAEPaginaCatu}>
+            ANTERIOR{" "}
+          </button>
+        </div>
+        <div className="">
+          <button className="button" onClick={irAEjercicios2}>
+            SIGUIENTE{" "}
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-
-</div>
-
-            <div className="divtitulo">EJERCICIO</div>
-            <div className="divvideo">
-            
-               <button className="button" style={{fontSize:15}} data-bs-toggle="modal" data-bs-target="#exampleModal"> <img src={playImg} width="15%" /> VER VIDEO</button>
-               
-            </div>
-            <div className="divcontenido">
-                
-                <div className="divImagen">
-                    <img src={niniosImg} width="100%" />
-                </div>
-                <div className="divf">
-                <div className="divtexto" >
-                    <br />
-                    
-                    Para mantener un cuerpo sano es importante hacer ejercicio todos los días</div>
-                </div>
-            </div>
-            <div className="divanterior">
-            <button className="button" onClick={irAEPaginaCatu}>ANTERIOR </button>
-            </div>
-            <div className="divsiguiente">
-            <button className="button" onClick={irAEjercicios2}>SIGUIENTE </button>
-            </div>
-            
-            
-        </div>
-
-        
-    )
-}
-
+  );
+};
--- src/pages/Lesiones2Page.tsx
+++ src/pages/Lesiones2Page.tsx
@@ -1,83 +1,72 @@
-import React from 'react'
-import '../pages/estilosFj.css'
+import React from "react";
+import "../themes/estilosFj.css";
 
-import im4 from '../assets/dolor4.png';
-import im5 from '../assets/dolor5.png';
-import im6 from '../assets/dolor6.png';
+import im4 from "../assets/dolor4.png";
+import im5 from "../assets/dolor5.png";
+import im6 from "../assets/dolor6.png";
 import { useHistory } from "react-router-dom";
 
-
-
 export const Lesiones2Page = () => {
-    const history = useHistory();
-    const irAnterior=()=>{
-        history.push("/Lesiones");
-      }
-const irSiguiente=()=>{
-    history.push("/ManipulacionCargas");
-  }
-    return (
-        <div id="fondo">
-       <div id="contenedor">
-           <div className="title">
-           <h1>
-           Principales Lesiones
-           </h1>
-           </div>
-       
-       <div id="c1">
-        <h3>
-        Hernia Discal
-        </h3>
-        <section>
-        Afección caracterizada
-         por un problema en el disco cartilaginoso
-          ubicado entre los huesos de la columna vertebral.
-            </section>
-            <img  id="hernia" src={im5}
-            >
+  const history = useHistory();
+  const irAnterior = () => {
+    history.push("/Lesiones");
+  };
+  const irSiguiente = () => {
+    history.push("/cargaD");
+  };
+  return (
+    <div id="fondo">
+      <div id="contenedor">
+        <div className="title">
+          <h1>Principales Lesiones</h1>
+        </div>
 
-            </img>
-       </div>
-       <div id="c2">
-       <h3>
-            Fractura
-        </h3>
-        <section>
-        Puede haber pérdida de funcionalidad.
-El tratamiento generalmente implica volver a colocar el hueso en su lugar e, inmovilizarlo con un yeso o una férula
-            </section>
-            <img  src={im6}
-            >
-
-            </img>
-       </div>
-       <div id="c3">
-       <h3>
-            Ciatica
-        </h3>
-        <section>
-        Dolor que se extiende desde la espalda baja hasta una o ambas piernas.
-Suele ocurrir cuando una hernia de disco o un espolón en la columna vertebral presionan el nervio.
-            </section>
-            <img  src={im4}
-            >
-
-            </img>
-       </div>
-      
-       <div className="container">  
-  <button   onClick={irAnterior}  className="button" id="anterior">Anterior</button>
-</div>  
-<div className="container">  
-  <button   onClick={irSiguiente} className="button" id="siguiente">Siguiente</button>
-</div>  
-      
-       </div>
-
-
-   
-
-       </div>
-    )
-}
+        <div id="c1">
+          <h3>Hernia Discal</h3>
+          <section>
+            Afección caracterizada por un problema en el disco cartilaginoso
+            ubicado entre los huesos de la columna vertebral.
+          </section>
+          <img id="hernia" src={im5}></img>
+        </div>
+        <div id="c2">
+          <h3>Fractura</h3>
+          <section>
+            Puede haber pérdida de funcionalidad. El tratamiento generalmente
+            implica volver a colocar el hueso en su lugar e, inmovilizarlo con
+            un yeso o una férula
+          </section>
+          <img className="img" src={im6}></img>
+        </div>
+        <div id="c3">
+          <h3>Ciatica</h3>
+          <section>
+            Dolor que se extiende desde la espalda baja hasta una o ambas
+            piernas. Suele ocurrir cuando una hernia de disco o un espolón en la
+            columna vertebral presionan el nervio.
+          </section>
+          <img className="img" src={im4}></img>
+        </div>
+        <div
+          className="container-fluid"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 480,
+          }}
+        >
+          <div className="container">
+            <button onClick={irAnterior} className="button" id="anterior">
+              Anterior
+            </button>
+          </div>
+          <div className="container">
+            <button onClick={irSiguiente} className="button" id="siguiente">
+              Siguiente
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
