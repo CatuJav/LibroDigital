e97002889646ecee1d38b6b91785b8a8557e783c
Javier <jalcoser1996@gmail.com>
2021-07-29 16:45:36 -0500

    Empaquetar en archivo msi

---
+++ build_installer.js
@@ -0,0 +1,26 @@
+const {MSICreator}= require('electron-wix-msi');
+const path= require('path');
+
+const APP_DIR = path.resolve(__dirname,'./libro-win32-x64');
+const OUT_DIR = path.resolve(__dirname,'./windows_installer');
+
+const msiCreator= new MSICreator({
+    appDirectory:APP_DIR,
+    outputDirectory:OUT_DIR,
+    description:"Libro acerca del cuidado del cuerpo",
+    exe:'libro',
+    manufacturer:'Proyecto de vinculación',
+    name:'Cuidados de mi cuerpo',
+    version:'1.0.0',
+    appIconPath:'./libro-win32-x64/resources/app/public/iconoInicial.ico',
+    ui:{
+        chooseDirectory:true
+    }
+});
+
+async function build() {
+	await msiCreator.create();
+	await msiCreator.compile();
+}
+
+build().catch(console.error);
